a01ff3ad7bd6e800132322097bc37cbeafdce0bd
diyor011 <diyorraxmatullaev3@gmail.com>
2026-06-20 16:48:03 +0500

    test

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './componetns/Navbar'
+import { Outlet } from 'react-router-dom'
+import Aside from './componetns/Aside'
+
+const App = () => {
+  return (
+    <div>
+
+      <Navbar />
+
+      <div className='flex'>
+        <Aside />
+        <div className='p-4'>
+          <Outlet />
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
