7c123395ee4febc0ad5a166a54baf812daf8008c
ubaydullayevmiraziz2012-tech <ubaydullayevmiraziz2012@gmail.com>
2026-06-23 15:12:51 +0500

    add aside and navbar

--- src/componetns/Aside.jsx
+++ src/componetns/Aside.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 const Aside = () => {
   return (
-    <div>Aside</div>
+    <div className=''>
+        <div className="drawer lg:drawer-open">
+  <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content flex flex-col items-center justify-center">
+    {/* Page content here */}
+    <label htmlFor="my-drawer-3" className="btn drawer-button lg:hidden">
+      Open drawer
+    </label>
+  </div>
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+    <ul className="menu bg-base-300 min-h-full w-80 p-4">
+      {/* Sidebar content here */}
+       <li><NavLink to={'/product'} >product</NavLink></li>
+            <li><NavLink to={'/users'} >users</NavLink></li>
+            <li><NavLink to={'/weather'} >weather</NavLink></li>
+    </ul>
+  </div>
+</div>
+    </div>
   )
 }
 
